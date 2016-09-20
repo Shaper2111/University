bc9a01e2875befd8ab9b22b9a14055d3e9bbb880
GolemXIV <Shaper2010@yandex.ru>
2016-09-20 17:09:07 +0400

    Realize entities and attributes

---
+++ src/main/java/com/haulmont/testtask/models/Student.java
@@ -0,0 +1,74 @@
+/*
+ * %W% %E% Firstname Lastname
+ *
+ * Copyright (c) 2016.
+ *
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ *
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.haulmont.testtask.models;
+
+import java.util.Date;
+
+public class Student extends Entity {
+    private String firstName;
+    private String lastName;
+    private String midName;
+    private Date birthDate;
+    private Integer groupNumber;
+
+    Student(Long id, String firstName, String lastName,
+            String midName, Date birthDate, Integer groupNumber) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.midName = midName;
+        this.birthDate = birthDate;
+        this.groupNumber = groupNumber;
+    }
+
+    String getFirstName() {
+        return firstName;
+    }
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    String getLastName() {
+        return lastName;
+    }
+
+    void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    String getMidName() {
+        return midName;
+    }
+
+    void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    Date getBirthDate() {
+        return birthDate;
+    }
+
+    void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    Integer getGroupNumber() {
+        return groupNumber;
+    }
+
+    void setGroupNumber(Integer groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+}
